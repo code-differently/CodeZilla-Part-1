468e1e6e5b571ada111f2d32ede657e2d67df06f
Shannon <shannoncarroll209@gmail.com>
2021-03-14 20:20:49 -0400

    upated problem

--- ProblemSet.js
+++ ProblemSet.js
@@ -7,7 +7,13 @@ class ProblemSet {
     and minutes with a colon.
     */
    timeConvert(num){
-       return num;
+       // calculate number of hours //divide number by 60
+       let hours = Math.floor(num/60);
+      //get remainder after dividing by 60 calculate mins
+       let mins = num% 60;
+       //create  an output.
+       let convertedTime = `${hours}:${mins}`;
+       return convertedTime;
    }
 
     /*
@@ -18,7 +24,20 @@ class ProblemSet {
     */
     countBigWords(input) {
         // code goes here
-        return null;
+        //split the  input in each character
+        let words = input.split(" ");
+        //count characters in each word
+        let bigWords=[];
+        //put all the words longer than 6 in an array
+        for (let i=0; i < words.length; i++ ) {
+            
+            if (words[i].length > 6) {
+                bigWords.push(words[i]);
+            }
+        }
+        //make an array
+       //count lenght of the array
+        return bigWords.length;
     }
 
     /*
@@ -28,9 +47,18 @@ class ProblemSet {
     * 0 tens, 1 twenty, 3 fifties, and 0 one hundreds
     */
     countingBills(input) {
-        return -1;
+        // create an array that represent demoninations [One, Five, Ten, Twenty, Fifty, Hundred]
+        let bills = [1, 5, 10, 20, 50, 100];
+        // create a money variable that starts with $0
+        let money = 0;
+        //iterate through array until all input number have reached the end
+        // add value of i * the denomination until end is reached 
+        for (let i = 0; i < input.length; i++){
+            money += (input[i] * bills[i]);
+        }
+        // return value of money
+        return money;
     }
-
     /**
      * Have the function flip(input) take the input parameter, comprised of As and Zs being passed and
      * return the string with each character flipped. For example: if the input string is "AAZZA" then your
@@ -38,7 +66,19 @@ class ProblemSet {
      */
 
     flip(input){
-        return null;
+        //  set empty variable 
+        let string = '';
+
+        //  split eatch letter, iterate through each letter. If the letter is A
+        //  change to Z and add it to the string array, if Z, change to A.
+        // condition ? exprIfTrue : exprIfFalse
+
+        input.split('').forEach(letter => {
+            string += letter === 'A' ? 'Z' : 'A';
+        });
+        console.log(string);
+      // return populated string with reverse output
+        return string;
     }
 
 }
